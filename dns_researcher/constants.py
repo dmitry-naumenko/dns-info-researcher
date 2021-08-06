9f23f15ae9ae1f45399d5d800a96c38009931730
Dmitry Naumenko <6pirule@gmail.com>
2021-08-06 13:47:02 +0200

    feat: add validation

--- dns_researcher/constants.py
+++ dns_researcher/constants.py
@@ -1,5 +1,9 @@
 """Constants."""
 from enum import Enum
+from typing import Final
+
+MIN_URL_LENGTH: Final[int] = 5
+MAX_URL_LENGTH: Final[int] = 50
 
 
 class DnsTypes(Enum):
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 """Main."""
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 from dns_researcher import get_mx_response
+from dns_researcher.constants import MAX_URL_LENGTH, MIN_URL_LENGTH
 from dns_researcher.responses import MxResponse
 
 app = FastAPI()
 
 
-@app.get("/v1/get_mx_info/{domain_name}")
-def get_mx_info(domain_name: str) -> dict[str, list[MxResponse]]:
+@app.get("/v1/get_mx_info/")
+def get_mx_info(
+    domain_name: str = Query(..., min_length=MIN_URL_LENGTH, max_length=MAX_URL_LENGTH)
+) -> dict[str, list[MxResponse]]:
     """Get mx info.
 
     Args:
