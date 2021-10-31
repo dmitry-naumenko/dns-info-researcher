5118b5554dd82355c0ee6e9538f484a185e868cd
Dmitry Naumenko <6pirule@gmail.com>
2021-10-31 17:20:51 +0200

    refactor: refactor api

--- app/api/api_v1/endpoints/aaaa_record.py
+++ app/api/api_v1/endpoints/aaaa_record.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter
 from app.core.models.response import AAAAResponse
 from app.core.services import researcher
 
-from ..query_parameters import domain_name_parameter
+from ...query_parameters import domain_name_parameter
 
 router = APIRouter()
 
